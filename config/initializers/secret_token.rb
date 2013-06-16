d3f8ae3a23e5e4f235adb2a0f9df41e8da30a076
Wayne <yo-rowe@sympatico.ca>
2013-06-15 23:56:40 -0400

    First Draft
    Initial Test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newsbeatle::Application.config.secret_token = 'eff9ba53a88bcfa299688cb0ffe004649c3a361148756e5ee2bb3ca207afab6062b4420d677d42fe2d3fff23db0549e9a8ca85517222f98d1a09fdfb9c6233f9'
---
+++ test/functional/sellers_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SellersControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
